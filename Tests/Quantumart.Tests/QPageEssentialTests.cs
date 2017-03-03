ac75574209d2fd60cd60364d35e1096255760f7e
Костюков Александр <kostyukov.alex@hotmail.com>
2017-03-03 15:14:00 +0300

    Fix tests project

---
+++ Tests/Quantumart.Tests/QPageEssentialTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.UI;
+using Quantumart.QPublishing.Pages;
+using Xunit;
+
+namespace Quantumart.Tests
+{
+    public class QPageEssentialTests
+    {
+        [Fact]
+        public void AddValue_WithNullValue()
+        {
+            var page = new QPageEssential(new Page());
+            page.FillValues();
+            page.AddValue("key", null);
+
+            Assert.Null(page.DirtyValue("key"));
+            Assert.Null(page.StrValue("key"));
+            Assert.Null(page.Value("key"));
+            Assert.Empty(page.Value("key1"));
+        }
+    }
+}
