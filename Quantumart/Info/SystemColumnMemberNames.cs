dbd838416a0e84c84b9a428a0d3746912f40256b
Anastasya Kuznetsova <KuznetsovaA@quantumart.ru>
2017-06-27 11:22:39 +0300

    adding column names as member for validation LINQ properties

---
+++ Quantumart/Info/SystemColumnMemberNames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantumart.QPublishing.Info
+{
+    public class SystemColumnMemberNames
+    {
+        public static readonly string Id = "Id";
+        public static readonly string StatusTypeId = "StatusTypeId";
+        public static readonly string Archive = "Archive";
+        public static readonly string Visible = "Visible";
+        public static readonly string LastModifiedBy = "LastModifiedBy";
+        public static readonly string Created = "Created";
+        public static readonly string Modified = "Modified";
+    }
+}
