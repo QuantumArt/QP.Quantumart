2191bfdfbee1e9e4f9cbdfcc4e2561c6125f3b3a
Sergey Koryakin <KoryakinS@quantumart.ru>
2017-12-20 18:34:43 +0300

    add setting validation #113064

---
+++ Quantumart/Authentication/AuthenticationException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Quantumart.QPublishing.Authentication
+{
+    public class AuthenticationException : Exception
+    {
+        public AuthenticationException(string message) : base(message)
+        {
+
+        }
+    }
+}
